2519e39237598525f3055e181a23b645fe292034
hojun <asnhojun@gmail.com>
2023-12-20 14:32:39 +0900

    2023.12.20 14:32 fix

--- Randomizer.py
+++ Randomizer.py
@@ -1,4 +1,4 @@
-# 2023.12.15 Fri edited
+# 2023.12.20 Wed edited
 import gc
 import copy
 import math
@@ -15,50 +15,45 @@ class ODE(nn.Module):
     def __init__(self,nptl:int,mu:float,spring:float,tau:float):
         super(ODE,self).__init__()
         self.N = nptl   # the number of particle
-        self.μ = mu     # drift
+        self.μ = mu     # motility
         self.τ = tau    # self propulsion decay rate
         self.K = spring # spring constant
 
-    # def Domain(self,X):
-    #     """
-    #     (0, system_size): 0th domain, (-system_size,0): -1st domain, ..., (system_size, 2*system_suze): 1st domain
-    #     """
-    #     with torch.no_grad():
-    #         rn = X[...,:self.N]
-    #         domain_id = (rn/system_size).to(torch.int64) # 입자가 어떤 domain에 들어가 있는가?
-    #     return domain_id
-
-    def Distance(self,X):
-        rn = X[...,:self.N]
-        drp = torch.roll(rn,shifts=(0,-1),dims=(0,1))
-
-            
-
-
-
-        return rn
-
-    
-
     def forward(self,X):
         with torch.no_grad():
-            rn = X[...,:self.N]
-
-
-        return 0
+            rn = X[...,:self.N]; an = X[...,self.N:]
+            rnp = torch.roll(rn,shifts=(0,-1),dims=(0,1)); rnm = torch.roll(rn,shifts=(0,+1),dims=(0,1))
+            dr1 = rnp - rn; dr2 = rn - rnm
+            Fr = self.μ*self.K(dr1-dr2)+an; Fa = -an/self.τ
+        return torch.cat([Fr,Fa],dim=-1)
 
 class SDE(nn.Module):
-    def __init__(self,ode,kbT:float,Da:float):
+    sde_type = "stratonovich"; noise_type = "general"
+    def __init__(self,ode,nptl:int,kbT:float,Da:float,diffusion_matrix):
         super(SDE,self).__init__()
-        self.deterministic = ode
-        self.kbT = kbT
-        self.Da = Da
-    
-    def forward(self,X):
-        return 0
+        self.F = ode
+        self.N = nptl             # the number of particle
+        self.kbT = kbT            # thermal fluctuation
+        self.Da = Da              # self-propulsion diffusion coefficient
+        self.D = diffusion_matrix # diffusion matrix (it is matrix)
 
-def Run(시간, 초기조건, 파라미터 조건, cpu or GPU?):
-    ode = ODE(); sde = SDE()
-    Noise = BrownianInterval()
-    final = sdeint(sde,)
-    return copy.deepcopy(final)
+    def drift(self,t,X):
+        return self.F(X)
+    
+    def diffusion(self,t,X):
+        return self.D.expand([X.shape[0],2*self.N,2*self.N])
+
+def Run(dt,T,X_initial,mu,spring,tau,kbT,Da,diffusion_matrix,device):
+    # X_initial.shape = (n_traj, 2*nptl)
+    # dt : time step, T : time length
+    n_traj = len(X_initial[:,0]); n_ptl = int(len(X_initial[0,:])/2)
+    # Time : initialization time
+    Time = dt*torch.tensor([t for t in range(int(T/dt)+1)]).to(device)
+    # what system I want to solve?
+    ode = ODE(n_ptl,mu,spring,tau).to(device); sde = SDE(ode,n_ptl,kbT,Da,diffusion_matrix).to(device)
+    if   sde.sde_type == "ito":          sde_method = "euler"
+    elif sde.sde_type == "stratonovich": sde_method = "heun"
+    # Solve the stochastic differential equations
+    bm = BrownianInterval(t0=0.,t1=T+dt,dt=dt,size=(n_traj,2*n_ptl),device=device,cache_size=None)
+    X_final = sdeint(sde,X_initial,Time,bm=bm,dt=dt,method=sde_method,names={'drift':'drift','diffusion':'diffusion'})[-1]
+    return copy.deepcopy(X_final)
